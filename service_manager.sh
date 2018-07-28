9cca158050ea315489f2581c83bb5648002dfad4
leogtzr <leogutierrezramirez@gmail.com>
2018-07-28 11:34:08 -0600

    Adding code to list all services and only running services.

--- service_manager.sh
+++ service_manager.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 readonly ERROR_CONFLICTING_OPTIONS=80
 
@@ -20,30 +21,40 @@ list_running_services() {
 show_help() {
 cat <<HELP_TEXT
 
-    ${0} [-l|--list] | [-h|--help] | [-d|--disable] | [-e|--enable]
+    ${0} [-l|--list] | [-h|--help] | [-d|--disable] | [-e|--enable] | [-a|--all]
+
+    -l|--list 			List running services.
+    -e|--enable 		Shows a menu to enable disabled services.
+    -d|--disable 		Shows a menu to disable running services.
+    -a|--all 			List all services.
 
 HELP_TEXT
 }
 
 validate_flags() {
-	local -r FLAGS_SUMMARY=$((enable_flag + disable_flag + list_flag))
+	local -r FLAGS_SUMMARY=$((enable_flag + disable_flag + list_flag + all_flag))
 	# FLAGS_SUMMARY == 0 when there are no options ... 
 	(((FLAGS_SUMMARY == 0) || (FLAGS_SUMMARY == 1)))
 }
 
-
-readonly OPTS=$(getopt -o h,l,e,d --long list,enable,disable,help -- "${@}" 2> /dev/null)
+readonly OPTS=$(getopt -o h,l,e,d,a --long list,all,enable,disable,help -- "${@}" 2> /dev/null)
 
 eval set -- "${OPTS}"
 list_flag=0
 enable_flag=0
 disable_flag=0
+all_flag=0
 
 while true; do
     case "${1}" in
 
         -l|--list)
             list_flag=1
+            shift
+            ;;
+
+        -a|--all)
+            all_flag=1
             shift
             ;;
 
@@ -76,6 +87,12 @@ done
 if ! validate_flags; then
 	echo "${0}: error, conflicting options."
 	exit ${ERROR_CONFLICTING_OPTIONS}
+fi
+
+if ((list_flag == 1)); then
+	list_running_services
+elif ((all_flag == 1)); then
+	list_all_services
 fi
 
 exit 0
